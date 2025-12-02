aace36912d65d78b7f6af752faec720b571e3c4a
Lupino-1 <lllupino111@gmail.com>
2025-12-02 20:11:39 +0100

    1.0

---
+++ src/main/java/dev/rajce/ketchupStats/KetchupStats.java
@@ -0,0 +1,17 @@
+package dev.rajce.ketchupStats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KetchupStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
